796de30cb64cc8c7f02ed8846d4df63ad15fc52d
SlaneYang <904509221@qq.com>
2016-05-17 18:46:30 +0800

    完成javascript部分，写的有点乱，第一次用jquery

--- jquery轮播图/scripts/app/banner.js
+++
@@ -1,1 +0,0 @@
-console.log("banner.js");
--- jquery轮播图/scripts/lib/config.js
+++ jquery轮播图/scripts/lib/config.js
@@ -6,10 +6,67 @@ require.config({
     // 不要指定".js"后缀，因为paths可以是一个目录
     paths: {
         'jquery': 'jquery',
-        'banner': '../app/banner',
     }
 });
 
-require(['jquery','banner'],function(){
-    console.log("Hello, I'm powered by jQuery " + $().jquery );
+require(['jquery'],function(){
+    $(function () {
+        var conatiner = $(".container");
+        var imageWrap = $("#image-list");
+        var imageNum = 4;
+        var imageWidth = 960;
+        var circleList = $("#image-list + ul");
+        var pre = $("#pre");
+        var next = $("#next");
+        var index = 0;
+        var interval = 3000;
+        var timer;
+
+        play();
+        conatiner.hover(stop, play);
+
+        pre.bind("click", function () {
+            index--;
+            animate (imageWidth);
+        });
+
+        next.bind("click", function () {
+            index++;
+            animate (-imageWidth);
+        });
+
+        circleList.children().bind('click', function () {
+                var thisIndex = $(this).index();
+                var offset = -imageWidth * (thisIndex - index);
+                animate(offset);
+                index = thisIndex;
+                showButton();
+       });
+
+        function animate (offset) {
+            var newLeft = parseInt(imageWrap.css("left")) + offset;
+            if (index == imageNum) {
+                index = 0;
+                newLeft = 0;
+            } else if (index == -1) {
+                index = 3;
+                newLeft = -imageWidth * (imageNum-1) + "px";
+            }
+            imageWrap.css("left",newLeft);
+            showButton();
+        }
+        function showButton () {
+            $(circleList.children().eq(index)).addClass("circle-selected")
+                .siblings().removeClass("circle-selected");
+        }
+        function play () {
+           timer = setTimeout(function () {
+               next.trigger('click');
+               play();
+           }, interval);
+       }
+       function stop () {
+           clearTimeout(timer);
+       }
+    });
 });
