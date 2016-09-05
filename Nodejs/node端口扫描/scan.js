17cb5911fa84daa707ffe12f1ee1ad27168ac29a
SlaneYang <904509221@qq.com>
2016-09-05 20:57:08 +0800

    应用let

--- Nodejs/node端口扫描/scan.js
+++ Nodejs/node端口扫描/scan.js
@@ -1,23 +1,23 @@
-var result = [];
+let result = [];
 scan('www.slane.cn', 1, 65535, function (result) {
     console.log('开启端口:');
-    for (var i = 0; i < result.length; i++) {
+    for (let i = 0; i < result.length; i++) {
         console.log('端口:' + result[i]);
     }
 });
 
 
 function scan (host, start, end, callback) {
-    var net = require('net');
-    var count = end - start;
+    let net = require('net');
+    let count = end - start;
     console.time('port scan time');
 
-    for (var i = start; i <= end; i++){
+    for (let i = start; i <= end; i++){
         console.log('正在扫描' + host + ':' +i);
         if (i == end) {
             console.log('等待扫描结果中......');
         };
-        var item = net.connect({
+        let item = net.connect({
             host:host,
             port:i
         },
