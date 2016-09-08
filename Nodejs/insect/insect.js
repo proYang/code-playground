77b45d55bad5c40e482094834dd3230e30da6c00
SlaneYang <904509221@qq.com>
2016-09-08 23:05:10 +0800

    用Promise优化图片爬虫

--- Nodejs/insect/insect.js
+++ Nodejs/insect/insect.js
@@ -1,10 +1,10 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var path = require('path');
-var fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const path = require('path');
+const fs = require('fs');
 // var async = require('async');
-var pageUrls = [];
-var index = 0;
+let pageUrls = [];
+let index = 0;
 // for(var i=130480 ; i<= 136699; i++){
 	pageUrls.push('https://isux.tencent.com/phonegraphy.html');
 	// pageUrls.push('https://ideras.me/blog/pure-css-columns-waterfall.html');
@@ -28,7 +28,7 @@ var timer = setInterval(function () {
 		clearInterval(timer);
 		console.log("have finished");
 	}
-}, 5000);
+}, 1000);
 // 并发连接数的计数器
 // var concurrencyCount = 0;
 // var fetchUrl = function (url, callback) {
@@ -54,7 +54,7 @@ function originRequest(requrl) {
 	 	if (!error && response.statusCode == 200) {
 	    	// console.log(body);    //返回请求页面的HTML
 	    	console.log("loaded HTML");
-	    	acquireData(body);
+			praseData(body);
 		}
 	});
 }
@@ -66,21 +66,32 @@ function originRequest(requrl) {
 // 	}
 // })
 
-
-function acquireData(data) {
-    var $ = cheerio.load(data);
- 
-    var images = $('img').toArray();
-    console.log(images.length);
-    var len = images.length;
-    for (var i=0; i<len; i++) {
-        var imgsrc = images[i].attribs.src;
-        console.log(imgsrc);
-        var filename = parseUrlForFileName(imgsrc);  //生成文件名
-        downloadImg(imgsrc,filename,function() {
-            console.log(filename + ' done');
-        });
-    }
+//
+// function acquireData(data) {
+//     var $ = cheerio.load(data);
+//     var images = $('img').toArray();
+//     console.log(images.length);
+//     var len = images.length;
+//     for (var i=0; i<len; i++) {
+//         var imgsrc = images[i].attribs.src;
+//         var filename = parseUrlForFileName(imgsrc);  //生成文件名
+//         downloadImg(imgsrc,filename).then(function (url) {
+//
+// 		})
+//     }
+// }
+function praseData(data) {
+	let $ = cheerio.load(data);
+	let images = $('img').toArray();
+	let imgLength = images.length;
+	console.log('共' + imgLength + '图片');
+	for (let i=0; i<imgLength; i++) {
+		let imgsrc = images[i].attribs.src;
+		let filename = parseUrlForFileName(imgsrc);
+		downloadImg(imgsrc, filename).then(function () {
+			console.log('NO' + i +' done');
+		});
+	}
 }
  
 function parseUrlForFileName(address) {
@@ -88,16 +99,33 @@ function parseUrlForFileName(address) {
     return filename;
 }
  
-var downloadImg = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-	    // console.log('content-type:', res.headers['content-type']);  //这里返回图片的类型
-	    // console.log('content-length:', res.headers['content-length']);  //图片大小
-	    if (err) {
-	        console.log('err: '+ err);
-	        return false;
-	    }
-	    console.log('res: '+ res);
-	    request(uri).pipe(fs.createWriteStream('images/'+filename)).on('close', callback);  //调用request的管道来下载到 images文件夹下
-    });
-};
- 
+// var downloadImg = function(uri, filename, callback){
+//     request.head(uri, function(err, res, body){
+// 	    // console.log('content-type:', res.headers['content-type']);  //这里返回图片的类型
+// 	    // console.log('content-length:', res.headers['content-length']);  //图片大小
+// 	    if (err) {
+// 	        console.log('err: '+ err);
+// 	        return false;
+// 	    }
+// 	    console.log('res: '+ res);
+// 	    request(uri).pipe(fs.createWriteStream('images/'+filename)).on('close', callback);  //调用request的管道来下载到 images文件夹下
+//     });
+// };
+ const downloadImg = function (url, filename) {
+ 	return new Promise(function (resolve, reject) {
+		request.head(url, function (err, res, body) {
+			if (err) {
+				reject(err);
+			} else {
+				request(url)
+					.pipe(fs.createWriteStream('images/' + filename))
+					.on('finish', function() {
+						resolve();
+					})
+					.on('error', function(err){
+						console.log("下载失败" + err);
+					});
+			}
+		})
+	});
+ }
