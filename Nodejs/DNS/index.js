274c15518da5860a3e0082093ef365a6e9f367e9
SlaneYang <904509221@qq.com>
2016-09-17 16:10:35 +0800

    重构Dns

--- Nodejs/DNS/index.js
+++ Nodejs/DNS/index.js
@@ -1,22 +1,43 @@
-var http = require('http'),
-    dns = require('dns');
+const  http 	= 	require('http'),
+    	dns 	= 	require('dns');
 
 
 http.createServer(function (req, res) {
-    getDns('www.slane.cn', function (hostname, addresses) {
-        console.log(hostname+'\nip:'+addresses);
-        var message = hostname + '\nip:' + addresses;
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(message);
-    });
+
+    getDns('www.slane.cn').then(
+    	function (hostname, addresses) {
+
+	        console.log(hostname+'\nip:'+addresses);
+	        var message = hostname + '\nip:' + addresses;
+	        res.writeHead(200, {'Content-Type': 'text/plain'});
+	        res.end(message);
+
+	    }, function (err) {
+
+	    	console.log("出错了！");
+	    	console.log(err);
+	    	
+	    })
+
 }).listen(3000, "127.0.0.1");
+
 console.log('server is run 127.0.0.1:3000');
 
-function getDns(hostname, callback) {
-    dns.resolve4(hostname, function (err, addresses) {
-        if(!addresses){
-            addresses = "无法解析";
-        }
-        callback(hostname, addresses);
-    })
+function getDns(hostname) {
+	return new Promise(function (resolve, reject) {
+	
+	    dns.resolve4(hostname, function (err, addresses) {
+
+	    	if (err) {
+	    		reject(err);
+	    	}
+
+	        if(!addresses){
+	            addresses = "无法解析";
+	        }
+
+	        resolve(hostname, addresses);
+	    })
+	
+	});
 }
