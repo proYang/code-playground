a82bd4d473fd7a8bdbbc6fdb00dc6713b6e5acc9
SlaneYang <904509221@qq.com>
2017-04-06 20:42:39 +0800

    数组去重:art:

--- 数组去重实例/arr.js
+++ 数组去重实例/arr.js
@@ -1,13 +1,13 @@
 function testArr() {
-	var arr2 = [1,2,3,4,5,6,7,8,9,0],
-		arr1 = [2,4,6,8,0],
+	var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+		arr1 = [2, 4, 6, 8, 0],
 		res = [];
 	for (var i = 0; i < arr1.length; i++) {
 		var obj = arr1[i];
 		var isExist = true;
 		for (var j = 0; j < arr2.length; j++) {
 			var aj = arr2[j];
-			if(aj == obj) {
+			if (aj == obj) {
 				isExist = false;
 				break;
 			}
@@ -19,3 +19,17 @@ function testArr() {
 	console.log(res);
 }
 testArr();
+
+function removeRepetition(arr) {
+	var obj = {}
+	var data = []
+	arr.forEach(function (item) {
+		if (!obj[item]) {
+			obj[item] = true
+			data.push(item)
+		}
+	})
+	return data
+}
+var arrD = removeRepetition([1, 1, 1, 1, 2, 3, 4, 3, 2, 0])
+console.log(arrD)
