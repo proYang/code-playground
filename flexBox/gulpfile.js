3360b064773c936179943527de5f66a605be60e7
SlaneYang <904509221@qq.com>
2017-07-17 17:24:03 +0800

    flex

--- flexBox/gulpfile.js
+++ flexBox/gulpfile.js
@@ -1,12 +1,12 @@
-var gulp         = require('gulp')
+var gulp = require('gulp')
 var autoprefixer = require('autoprefixer');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('autoprefixer', function () {
     return gulp.src('./*.css')
         .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer({ browsers: ['last 2 Chrome versions', 'Android >= 4.0'] }) ]))
+        .pipe(postcss([autoprefixer({ browsers: ['last 2 Chrome versions', 'Android >= 4.0'] })]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dest'));
 });
